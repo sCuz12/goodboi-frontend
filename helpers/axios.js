da0742e37d8a026811c90e9037fb9126a32f8cd3
sCuz12 <georgex8@gmail.com>
2022-06-20 11:25:21 +0300

    add port to request

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://164.92.128.233",
+  baseURL: "http://164.92.128.233:8000",
   headers: { "Access-Control-Allow-Origin": "*" },
 });
 
