3def36386538b2f53e982c290142454979ad5da6
sCuz12 <georgex8@gmail.com>
2022-06-21 20:00:55 +0300

    Adding application/json headers

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
+//https://api.goodboi.com.cy
 const axiosInstance = axios.create({
   baseURL: "https://api.goodboi.com.cy",
-  headers: { "Access-Control-Allow-Origin": "*" },
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
