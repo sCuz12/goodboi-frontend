c94f664b1618e2888dbe1b8aaec798df39d50016
sCuz12 <georgex8@gmail.com>
2022-06-24 10:47:22 +0300

    add new side nav for shelter

--- components/Nav/shelter/ShelterSideNav.js
+++ components/Nav/shelter/ShelterSideNav.js
@@ -2,7 +2,7 @@ import React from "react";
 import { AiOutlineDashboard, AiOutlineProfile } from "react-icons/ai";
 import { IoCreateOutline } from "react-icons/io5";
 import SideBarItem from "./Items/SideBarItem";
-
+import { SiDatadog } from "react-icons/si";
 function ShelterSideNav() {
   return (
     <div className="flex-initial h-screen p-5 pt-20 bg-white shadow-md pt-50 w-60 mt-28">
@@ -29,6 +29,13 @@ function ShelterSideNav() {
           }
           name="Shelter Profile"
           url="/shelter/profile/update"
+        />
+        <SideBarItem
+          icon={
+            <SiDatadog className="block float-left mr-2 text-xl text-black cursor-pointer" />
+          }
+          name="My Listings"
+          url="/shelter/mylistings/view"
         />
         <SideBarItem
           icon={
