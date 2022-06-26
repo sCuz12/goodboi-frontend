bb8f9f6439c0477a7fb58bc5c4ad2f39ac1a530a
sCuz12 <georgex8@gmail.com>
2022-06-26 20:20:23 +0300

    change url from api to backend

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //https://api.goodboi.com.cy
 const axiosInstance = axios.create({
-  baseURL: "https://api.goodboi.com.cy",
+  baseURL: "https://backend.goodboi.com.cy",
   headers: {
     "Access-Control-Allow-Origin": "*",
     Accept: "application/json",
