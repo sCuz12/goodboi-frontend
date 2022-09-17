e8c84337961d0ff0e43e0c0ee450af66cdbc9b72
sCuz12 <georgex8@gmail.com>
2022-09-17 16:55:54 +0300

    -Add sort functionality

--- pages/listings/animals.js
+++ pages/listings/animals.js
@@ -1,9 +1,13 @@
-import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import ListingCard from "../../components/Cards/ListingCard";
 import PinkCircle from "../../components/CustomImages/DogInCircle";
 import axiosInstance from "../../helpers/axios";
 import { MdSort } from "react-icons/md";
+import { BiSort } from "react-icons/bi";
+
+import { Button, Dropdown, Menu, message, Space, Tooltip } from "antd";
+
+import { DownOutlined } from "@ant-design/icons";
 
 import AbovePageSection from "../../components/Sections/AbovePageSection";
 import Pagination from "react-js-pagination";
@@ -19,6 +23,7 @@ function animals() {
   const [perPage, setPerPage] = useState();
   const [token, setToken] = useState("");
   const [filtersCollapse, setFiltersCollapse] = useState(false);
+  const [sortBy, setSortBy] = useState([]);
 
   useEffect(() => {
     const token = window.localStorage.getItem("token");
@@ -35,6 +40,10 @@ function animals() {
   useEffect(() => {
     filterListingsFromCities();
   }, [checkedCity]);
+
+  useEffect(() => {
+    sortListingsBy();
+  }, [sortBy]);
 
   /* Method gets all cities and set state*/
   const getCities = async () => {
@@ -71,6 +80,15 @@ function animals() {
       setTotalListings(data.data.total);
     }
   };
+  //sends add sort fields and send request
+  const sortListingsBy = async () => {
+    let formData = new FormData();
+    formData.append("sort", sortBy[0]);
+    formData.append("sortValue", sortBy[1]);
+    const { data } = await axiosInstance.post("/api/animals/dogs", formData);
+    setAnimalLIstings(data.data);
+    setTotalListings(data.data.total);
+  };
 
   const handleToggle = (id) => {
     const currentIndex = checkedCity.indexOf(id);
@@ -90,6 +108,56 @@ function animals() {
     console.log(filtersCollapse);
   };
 
+  //Handles the sort click of item
+  const MenuClickHandler = (e) => {
+    switch (e.key) {
+      case "name":
+        setSortBy([e.key, "asc"]);
+        break;
+      case "nameDesc":
+        setSortBy(["name", "desc"]);
+        break;
+      case "created_at":
+        setSortBy([e.key, "asc"]);
+        break;
+      case "created_at_desc":
+        setSortBy(["created_at", "desc"]);
+        break;
+      case "total_views":
+        setSortBy([e.key, "desc"]);
+        break;
+      default:
+        setSortBy(["name", "asc"]);
+    }
+  };
+
+  const sortMenu = (
+    <Menu
+      onClick={MenuClickHandler}
+      items={[
+        {
+          label: "Name Ascending",
+          key: "name",
+        },
+        {
+          label: "Name Descending",
+          key: "nameDesc",
+        },
+        {
+          label: "Oldest first",
+          key: "created_at",
+        },
+        {
+          label: "Newest first",
+          key: "created_at_desc",
+        },
+        {
+          label: "Most viewed",
+          key: "total_views",
+        },
+      ]}
+    />
+  );
   return (
     <div className="pt-0 lg:pt-20 lg:pb-20">
       {/* Header section */}
@@ -119,7 +187,19 @@ function animals() {
 
           {/* Listings */}
           <div className="p-4 lg:pr-8 lg:w-4/5 sm:px-16 ">
-            <h1 className="header_titles font-cherryBomb">All Dogs</h1>
+            <div className="flex ">
+              <h1 className="w-3/4 header_titles font-cherryBomb">All Dogs</h1>
+              {/**Sorting ui */}
+              <Dropdown overlay={sortMenu}>
+                <Button>
+                  <Space>
+                    <BiSort />
+                    Sorty By
+                    <DownOutlined />
+                  </Space>
+                </Button>
+              </Dropdown>
+            </div>
 
             {animalListings.length == 0 ? (
               <div className="flex justify-center w-full pt-20">
