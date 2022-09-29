23612821a1320a5a769a6c626a1547352d1fe6f4
sCuz12 <georgex8@gmail.com>
2022-09-29 07:56:15 +0300

    initial policy + data deletion pages

---
+++ pages/data/policy.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function policy() {
+  return (
+    <div className="flex justify-center w-full text-center align-center">
+      <h1 className="header_titles">policy</h1>;
+    </div>
+  );
+}
+
+export default policy;
