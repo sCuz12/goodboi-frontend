ad699be7cb8b0c48148f8e1bb13cdaab058e5f69
sCuz12 <georgex8@gmail.com>
2022-10-12 12:36:05 +0300

    -Single view of found dog

--- components/Cards/Listings/Found/FoundListingCard.js
+++ components/Cards/Listings/Found/FoundListingCard.js
@@ -12,7 +12,7 @@ function FoundListingCard({ item }) {
       <div className="items-center text-center">
         <div className="p-1.5 transition duration-300 ease-out transform cursor-pointer hover:scale-105">
           <div className="relative">
-            <Link href={`/listings/lost-dogs/view/${item.dog_id}`}>
+            <Link href={`/listings/found-dogs/view/${item.dog_id}`}>
               <Image
                 className="block object-cover shadow-inner rounded-2xl"
                 src={item.cover_image}
