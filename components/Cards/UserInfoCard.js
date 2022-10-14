cbb30b2b33898a1ed33576dee5239f2b853d4709
sCuz12 <georgex8@gmail.com>
2022-10-14 09:33:38 +0300

    -Redesign user card info

--- components/Cards/UserInfoCard.js
+++ components/Cards/UserInfoCard.js
@@ -1,26 +1,32 @@
 import React from "react";
 import Image from "next/image";
-import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineMail } from "react-icons/ai";
 
 function UserInfoCard({ user }) {
   return (
     <div className="flex flex-col sm:align-center">
       <div className="justify-center h-full sm:w-full lg:w-3/4 bg-roz rounded-xl lg:h-100">
-        <div className="flex flex-col justify-center h-full">
+        <div className="flex flex-col justify-center h-4/4">
           <h1 className="flex justify-center text-xl text-center">Owner</h1>
           <div className="flex justify-center w-full text-center rounded-3xlr">
             <Image
               src={user.cover_photo ? user.cover_photo : "/user_default.png"}
-              className="object-cover text-center rounded-3xl"
+              className="object-cover text-center rounded-full"
               height={200}
-              width={400}
+              width={200}
             />
           </div>
 
-          <div className="flex flex-col items-center pl-4">
-            <div className="flex flex-row gap-1 pt-3">
+          <div className="flex flex-col items-center pt-8 pl-4">
+            <div className="flex flex-row gap-1">
               <AiOutlineUser size={25} />
-              <p className="text-lg"> {user.first_name}</p>
+              <p className="text-lg">
+                {user.first_name} {user.last_name}
+              </p>
+            </div>
+            <div className="flex flex-row gap-1">
+              <AiOutlineMail size={25} />
+              <p className="text-lg">{user.email}</p>
             </div>
           </div>
         </div>
